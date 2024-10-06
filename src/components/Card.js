68813fbb882a3ac1ad5842daab174f1e3fc8ef99
Aman Singhal <amansinghal@Amans-MacBook-Air.local>
2024-10-06 17:41:51 +0530

    adding functionality to the buttons

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,34 +4,28 @@ import {FaQuoteLeft, FaQuoteRight } from 'react-icons/fa'
 const Card = (props) => {
     let review = props.review;
   return (
-    <div>
-        <div>
-            <img src={review.image}/>
+    <div className='md:relative flex flex-col '>
+        <div className='absolute top-[-7rem] z-[10] mx-auto '>
+            <img className='aspect-square rounded-full w-[140px] h-[140px] z-[25]' src={review.image}/>
         </div>
-        <div>
-            <p>{review.name}</p>
-        </div>
-        <div>
-            <p>{review.job}</p>
-        </div>
-        <div>
-            <FaQuoteLeft></FaQuoteLeft>
-        </div>
-        <div>
-            <p>{review.text}</p>
-        </div>
-        <div>
-            <FaQuoteRight></FaQuoteRight>
+        <div className="w-[140px] h-[140px] bg-violet-500 rounded-full absolute left-[12px] top-[-125px]"></div>
+
+        <div className='text-center mt-7'>
+            <p className='font-bold text-2xl capitalize'>{review.name}</p>
         </div>
 
-        <div>
-            <button>+</button>
-            <button>*</button>
+        <div className='text-center mt-7'>
+            <p className='text-violet-300 uppercase text-sm'>{review.job}</p>
         </div>
-        <div>
-            <button>
-                Surprise Me
-            </button>
+
+        <div className='text-center text-violet-400 mx-auto mt-5'>
+            <FaQuoteLeft></FaQuoteLeft>
+        </div>
+        <div className='text-center mt-4 text-slate-500'>
+            <p>{review.text}</p>
+        </div>
+        <div className='text-center text-violet-400 mx-auto mt-5'>
+            <FaQuoteRight></FaQuoteRight>
         </div>
          
     </div>
--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,11 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Card from './Card'
+import {FiChevronLeft, FiChevronRight} from 'react-icons/fi'
 
 const Testimonials = (props) => {
     let reviews = props.reviews;
+    let [index, setIndex] = useState(0);
+    function leftShiftHandler(){
+        if(index-1 < 0){
+            setIndex(reviews.length-1)
+        }else{
+            setIndex(index-1)
+        }
+    }
+    function rightShiftHandler(){
+        if(index+1 >= reviews.length){
+            setIndex(0)
+        }else{
+            setIndex(index+1)
+        }
+    }
+    function surpriseHandler(){
+        let randIndex = Math.floor(Math.random()*reviews.length)
+        setIndex(randIndex)
+    }
   return (
-    <div>
-        <Card review={reviews[0]}></Card>
+    <div className='w-[50vw] md:x-[700px] bg-white flex flex-col justify-center items-center mt-10 p-10 transition-all duration-300
+    hover:shadow-xl'>
+        <Card review={reviews[index]}></Card>
+
+        <div className='flex flex-row text-3xl gap-3 mt-5 text-violet-400 font-bold justify-center'>
+            <button onClick={leftShiftHandler} className='cursor-pointer hover:text-violet-500'><FiChevronLeft></FiChevronLeft></button>
+            <button onClick={rightShiftHandler} className='cursor-pointer hover:text-violet-500'><FiChevronRight></FiChevronRight></button>
+        </div>
+        <div>
+            <button onClick={surpriseHandler} className='bg-violet-400 hover:bg-violet-500 
+            transition-all duration-200 
+            rounded-md cursor-pointer px-10 py-2 font-bold text-white text-md'>
+                Surprise Me
+            </button>
+        </div>
     </div>
   )
 }
